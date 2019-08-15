28c1efa0e537a29d849fe37596556e5d20dcea24
Ruchira Munidasa <ruchira.lk@gmail.com>
2019-08-15 22:53:23 +0530

    Initial Commit

---
+++ RM.iOS.Core/RM_iOS_Core.h
@@ -0,0 +1,19 @@
+//
+//  RM_iOS_Core.h
+//  RM.iOS.Core
+//
+//  Created by Ruchira Munidasa on 8/15/19.
+//  Copyright © 2019 Ruchira Munidasa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RM_iOS_Core.
+FOUNDATION_EXPORT double RM_iOS_CoreVersionNumber;
+
+//! Project version string for RM_iOS_Core.
+FOUNDATION_EXPORT const unsigned char RM_iOS_CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RM_iOS_Core/PublicHeader.h>
+
+
