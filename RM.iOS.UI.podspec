006a32775e45a931a347458f10dd7770b9fc96e1
Ruchira Munidasa <ruchira.lk@gmail.com>
2019-09-06 00:05:15 +0530

    podspec 0.0.19

--- RM.iOS.UI.podspec
+++ RM.iOS.UI.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "RM.iOS.UI"
-  spec.version      = "0.0.17"
+  spec.version      = "0.0.19"
   spec.summary      = "Reusable UIKit Components."
 
   # This description is used to generate tags and improve search results.
--- RM.iOS.UI/CustomCell/CollectionView/RMProductCollectionViewCell.swift
+++ RM.iOS.UI/CustomCell/CollectionView/RMProductCollectionViewCell.swift
@@ -21,7 +21,7 @@ open class RMProductCollectionViewCell: UICollectionViewCell, RMInitializeView {
     
     public let productImageView: RMShadowImageView = {
        let imageView = RMShadowImageView(frame: .zero)
-        imageView.contentMode = ContentMode.scaleAspectFit
+        imageView.contentMode = ContentMode.scaleAspectFill
         imageView.image = UIImage(named: "TestImage")
         return imageView
     }()
@@ -38,7 +38,6 @@ open class RMProductCollectionViewCell: UICollectionViewCell, RMInitializeView {
         textView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         return textView
     }()
-    
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
