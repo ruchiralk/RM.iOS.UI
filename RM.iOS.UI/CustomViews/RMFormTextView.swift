4d4b86c6ae54fec529b6670cff18ff562c797d95
Ruchira Munidasa <ruchira.lk@gmail.com>
2019-09-25 00:56:24 +0530

    made RMTextView open

--- RM.iOS.UI/CustomViews/RMFormTextView.swift
+++ RM.iOS.UI/CustomViews/RMFormTextView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class RMFormTextView: UITextView {
+open class RMFormTextView: UITextView {
 
     var placeholder: String? {
         didSet {
@@ -17,7 +17,7 @@ class RMFormTextView: UITextView {
         }
     }
     
-    override var text: String! {
+    override open var text: String! {
         didSet {
             if text != placeholder {
                 self.textColor = defaultTextColor
@@ -27,7 +27,7 @@ class RMFormTextView: UITextView {
         }
     }
     
-    override var textColor: UIColor? {
+    override open var textColor: UIColor? {
         didSet {
             self.defaultTextColor = textColor
         }
@@ -41,7 +41,7 @@ class RMFormTextView: UITextView {
         self.font = UIFont.systemFont(ofSize: 16.0, weight: .light)
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
